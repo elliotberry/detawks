7e63fc574f2e75ab7e8f79e42360937886f5895d
E Berry <elliot@daddy.cat>
2023-07-23 23:49:16 -0400

    sort of added an rc file

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
-
 import yargs from 'yargs/yargs'
 import { fdir } from 'fdir'
-
+import path from 'path'
 import getFilePathInfo from './lib/file-path-info.js'
 import ignore from './lib/ignore.js'
 import processOneFile from './lib/process-one-file.js'
+import fs from 'fs'
 var opts
 
-const fdirBuilder = (globPattern) => {
-    !opts.silent && console.log(`glob pattern: ${globPattern}`)
-    let fns = []
-
-    if (opts.directories) {
-        fns.push('withDirs')
+var inputStr;
+const truncatePathIfPossible = (str) => {
+    if (inputStr.parentPath) {
+        if (str.startsWith(inputStr.parentPath)) {
+            return str.replace(inputStr.parentPath, inputStr.parent)
+        }
     }
-
-    let base = new fdir().glob(globPattern)
-
-    if (fns.length > 0) {
-        base = fns.reduce((acc, fn) => acc[fn](), base)
-    }
-
-    return base.withFullPaths().crawl().withPromise()
+    return str
 }
 
-const run = async (globPattern) => {
-   
+function isStringAGlobPattern(inputString) {
+    const globPatternRegex = /[*]/
+    return globPatternRegex.test(inputString)
+}
+const replaceAll = (str, find, replace) => {
+    return str.split(find).join(replace)
+}
+const replaceSquiglyWithHome = (str) => {
+    if (str.includes('~')) {
+        return replaceAll(str, '~', process.env.HOME)
+    }
+    return str
+}
+//verify input
+const getPathIfPath = async (globPath) => {
+    let ret = null
+    globPath = replaceSquiglyWithHome(globPath)
+    try {
+        let stats = await fs.promises.stat(globPath)
+        if (stats.isDirectory() || stats.isFile()) {
+            ret = {
+                path: path.resolve(globPath),
+                parent: path.dirname(globPath),
+                parentPath: path.resolve(path.dirname(globPath)),
+                type: stats.isDirectory() ? 'directory' : 'file',
+            }
+        }
+    } catch (e) {
+        ret = null
+    }
 
-    let files
+    if (ret === null) {
+        if (isStringAGlobPattern(globPath)) {
+            ret = { path: globPath, type: 'glob' }
+        }
+    }
+
+    return ret
+}
+
+const useFdir = async (globPattern) => {
+    var files = []
     if (opts.directories) {
-        !opts.silent &&  console.log(`including directories.`)
+        !opts.silent && console.log(`including directories.`)
         if (opts.maxDepth) {
             files = await new fdir()
                 .withFullPaths()
@@ -59,35 +90,52 @@ const run = async (globPattern) => {
         }
     }
 
+    return files
+}
+
+const run = async (globPattern) => {
+    inputStr = await getPathIfPath(globPattern)
+    if (inputStr === null) {
+        console.log(`invalid input: ${globPattern}`)
+        process.exit(1)
+    }
+    let files = await useFdir(globPattern)
+
     let filesFoundInGlob = files.length
-
-    !opts.silent && console.log(`found ${filesFoundInGlob} files in ${globPattern}.`)
-
     if (filesFoundInGlob === 0) {
-        !opts.silent &&  console.log(`thus, exiting.`)
+        !opts.silent && console.log(`thus, exiting.`)
         process.exit(0)
     }
+    !opts.silent &&
+        console.log(`found ${filesFoundInGlob} files in ${inputStr.path}.`)
 
-    let arrayOfFilePaths = await files.map((file) => getFilePathInfo(file, opts.fixExts))
+    let arrayOfFilePaths = await files.map((file) =>
+        getFilePathInfo(file, opts.fixExts)
+    )
     arrayOfFilePaths = await arrayOfFilePaths.filter((f) => f !== null)
 
     let lengthBefore = arrayOfFilePaths.length
     arrayOfFilePaths = await ignore(arrayOfFilePaths)
-    !opts.silent && console.log(`ignored ${lengthBefore - arrayOfFilePaths.length} files.`)
-    !opts.silent && console.log(
-        `${arrayOfFilePaths.length}/${files.length} ${
-            opts.dryrun ? `would` : `will`
-        } be renamed.`
-    )
+    !opts.silent &&
+        console.log(`ignored ${lengthBefore - arrayOfFilePaths.length} files.`)
+    !opts.silent &&
+        console.log(
+            `${arrayOfFilePaths.length}/${files.length} ${
+                opts.dryrun ? `would` : `will`
+            } be renamed.`
+        )
 
     if (opts.dryrun) {
-        !opts.silent &&  console.log(
-            arrayOfFilePaths
-                .map((f) => {
-                    return `${f.old} -> ${f.new}`
-                })
-                .join('\n')
-        )
+        !opts.silent &&
+            console.log(
+                arrayOfFilePaths
+                    .map((f) => {
+                        let oldPath = truncatePathIfPossible(f.old)
+                        let newPath = truncatePathIfPossible(f.new)
+                        return `${oldPath} -> ${newPath}`
+                    })
+                    .join('\n')
+            )
     } else {
         for await (let f of arrayOfFilePaths) {
             await processOneFile(f, opts.rename)
@@ -99,10 +147,19 @@ const main = async () => {
     var argv = yargs(process.argv.slice(2))
         .usage('Usage: $0 <glob> [options]')
         .boolean(['d', 'r', 'f', 'u', 's'])
-        .describe('s', 'silent mode; e.g no console.log describing new file names')
+        .describe(
+            's',
+            'silent mode; e.g no console.log describing new file names'
+        )
         .default('s', false)
-        .alias('u', 'fix unix file extensions created by backup scripts (e.g. .txt~)')
-        .describe('u', 'fix unix file extensions created by backup scripts (e.g. .txt~)')
+        .alias(
+            'u',
+            'fix unix file extensions created by backup scripts (e.g. .txt~)'
+        )
+        .describe(
+            'u',
+            'fix unix file extensions created by backup scripts (e.g. .txt~)'
+        )
         .default('u', false)
         .alias('d', 'dry run')
         .describe('d', 'dry run, showing what files would be renamed')
