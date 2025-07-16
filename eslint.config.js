6b2c3406ab6c68ec43444d75b22eed57cfd392da
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2025-07-15 21:02:21 -0400

    updated packages.

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,42 @@
-import importPlugin from 'eslint-plugin-import';
-import nodePlugin from 'eslint-plugin-n'; 
-import noSecrets from 'eslint-plugin-no-secrets'
-import packageJson from "eslint-plugin-package-json/configs/recommended";
+import js from '@eslint/js'
+import { importX } from 'eslint-plugin-import-x'
+import * as nodeDependenciesPlugin from 'eslint-plugin-node-dependencies'
+import packageJson from 'eslint-plugin-package-json'
 import perfectionist from 'eslint-plugin-perfectionist'
-import pluginPromise from 'eslint-plugin-promise'
-import pluginSecurity from 'eslint-plugin-security'
 import is from 'eslint-plugin-simple-import-sort'
-import sonarjs from "eslint-plugin-sonarjs";
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import sonarjs from 'eslint-plugin-sonarjs'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
+
 
 export default [
     eslintPluginUnicorn.configs['flat/recommended'],
-    pluginSecurity.configs.recommended,
-    packageJson,
-    nodePlugin.configs["flat/recommended-script"],
-    importPlugin.flatConfigs.recommended,
-    pluginPromise.configs['flat/recommended'],
+    js.configs.recommended,
+    ...nodeDependenciesPlugin.configs['flat/recommended'],
+    sonarjs.configs.recommended,
+    packageJson.configs.recommended,
+    importX.flatConfigs.recommended,
     {
         files: ['**/*.js'],
         languageOptions: {
             ecmaVersion: 2024,
             globals: {
-                clearTimeout: 'readonly',
+                Buffer: 'readonly',
                 console: 'readonly',
-                fetch: 'readonly',
+                "fetch": 'readonly',
                 process: 'readonly',
-                setTimeout: 'readonly',
-                timeOut: 'readonly',
+
             },
             sourceType: 'module',
         },
         plugins: {
-            'no-secrets': noSecrets,
             perfectionist,
             'simple-import-sort': is,
-            sonarjs
         },
 
         rules: {
-            
-            "import/no-unresolved": "off",
-            "n/no-unpublished-import": "off",
+            'import-x/default': 'warn',
+            'import-x/namespace': 'warn',
             'no-console': 'off',
-            'no-secrets/no-secrets': 'error',
             'no-unused-vars': 'warn',
             'perfectionist/sort-array-includes': 'warn',
             'perfectionist/sort-classes': 'warn',
@@ -56,18 +50,16 @@ export default [
             'perfectionist/sort-named-imports': 'warn',
             'perfectionist/sort-object-types': 'warn',
             'perfectionist/sort-objects': 'warn',
-            "security/detect-non-literal-fs-filename": "off",
-            "security/detect-object-injection": "off",
             'simple-import-sort/exports': 'warn',
             'simple-import-sort/imports': 'warn',
-            "sonarjs/cognitive-complexity": "warn",
+            "sonarjs/no-nested-template-literals": 'warn',
             'unicorn/consistent-function-scoping': 'warn',
             'unicorn/no-array-callback-reference': 'warn',
             'unicorn/no-null': 'warn',
             'unicorn/no-process-exit': 'off',
             'unicorn/prefer-switch': 'off',
             'unicorn/prefer-top-level-await': 'off',
-            'unicorn/prevent-abbreviations': 'warn',
+            'unicorn/prevent-abbreviations': 'warn'
         },
     },
 ]
