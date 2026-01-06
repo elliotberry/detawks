ae835de8857489b4fe8ca1375d1ca125f00349ba
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2026-01-05 22:22:05 -0500

    various

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import { logOldAndNewFilenames } from './lib/closest-parent-info.js'
 import getFilePathInfo from './lib/file-path-info.js'
 import ignore from './lib/ignore.js'
-import { filesFoundInfo, logIgnored, showProgress } from './lib/logging-functions.js'
+import { logIgnored, showProgress } from './lib/logging-functions.js'
 import { validateAndFormatInput } from './lib/path-validation.js'
 import processOneFile from './lib/process-one-file.js'
 import config from './lib/rc.js'
 import readDirectory from './lib/read-directory.js'
 import useFdir from './lib/use-fdir.js'
-
 
 // Process files in batches to control concurrency
 const processBatch = async (files, batchSize, processor) => {
@@ -16,16 +15,14 @@ const processBatch = async (files, batchSize, processor) => {
         const batch = files.slice(index, index + batchSize)
         const batchResults = await Promise.all(batch.map(processor))
         results.push(...batchResults)
-        
+
         // Show progress
         showProgress(Math.min(index + batchSize, files.length), files.length)
     }
     return results
 }
 
-
 const run = async (globPattern, userOptions) => {
-  
     const options = userOptions
         ? Object.assign({}, config, userOptions)
         : config
@@ -56,15 +53,13 @@ const run = async (globPattern, userOptions) => {
         files = [inputString.path]
     }
 
-    //console log that we got some shit to process.
-    filesFoundInfo(files, inputString, inputString.type)
-
     //get pending new/old filepaths given our current options loadout - optimized for concurrent processing
     const batchSize = options.batchSize || 50
     let arrayOfFilePaths = await processBatch(files, batchSize, getFilePathInfo)
     //filter out null paths (bugfix)
-    arrayOfFilePaths = arrayOfFilePaths.filter((f) => f !== null)
-
+    arrayOfFilePaths = arrayOfFilePaths.filter(
+        (f) => f !== null && f !== undefined && Object.keys(f).length > 0 && f.old !== f.new
+    )
     //Tell the user we ignored some garbage files
     const lengthBefore = arrayOfFilePaths.length
     arrayOfFilePaths = await ignore(arrayOfFilePaths)
@@ -72,14 +67,14 @@ const run = async (globPattern, userOptions) => {
 
     // Process files in batches for better performance and control
     if (!options.dryrun) {
-        await processBatch(arrayOfFilePaths, batchSize, file => processOneFile(file, options.rename))
+        await processBatch(arrayOfFilePaths, batchSize, (file) =>
+            processOneFile(file, options.rename)
+        )
     }
-    
+
     // Log results sequentially to maintain readable output order
     for await (const file of arrayOfFilePaths) {
         await logOldAndNewFilenames(file)
     }
-
-
 }
 export default run
